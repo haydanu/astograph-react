e231268fd369cfbb9847d8c4ac94671dad6c1710
herdanuyp <heh.danu@gmail.com>
2019-11-27 22:32:15 +0700

    Add chart

--- src/App.js
+++ src/App.js
@@ -1,52 +1,56 @@
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 
 import NavbarComponent from './components/Navbar';
 import BuySell from './components/BuySell';
 import MarketInfo from './components/MarketInfo';
 import OrderBook from './components/OrderBook';
 import MarketHistory from './components/MarketHistory';
+import TradeHeader from './components/TradeHeader';
+import Chart from './components/Chart';
 import './App.css';
 
 function App() {
   return (
     <div>
       <NavbarComponent />
-      <div className='asset-info'>
-        <Row>
-          <Col> {`<`} </Col>
-          <Col>XLM | Native</Col>
-          <Col>XLM | Native</Col>
-        </Row>
-      </div>
       <div className='template-content'>
-        <Row>
-          <Col>
-            <div className='first-column-wrapper'>
-              <BuySell />
-             
-              <div className='advertise'>
-                <h2>Try StellarX</h2>
-                <p>Join now for totally free</p>
-                <p>peer-to-peer trading</p>
-                <a
-                  href='https://www.stellarx.com/signup'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='signup-button black'>
-                  SIGNUP
-                </a>
+        <TradeHeader />
+
+        <div>
+          <Row>
+            <Col md='3'>
+              <div className='first-column-wrapper'>
+                <BuySell />
+
+                <div className='advertise'>
+                  <h2>Try StellarX</h2>
+                  <p>Join now for totally free</p>
+                  <p>peer-to-peer trading</p>
+                  <a
+                    href='https://www.stellarx.com/signup'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    className='signup-button black'>
+                    SIGNUP
+                  </a>
+
+                  <div>
+                    <small>* Yep, we even refund your network costs.</small>
+                  </div>
+                </div>
               </div>
-            </div>
-          </Col>
-          <Col>
-            <MarketInfo />
-          </Col>
-          <Col>
-            <OrderBook />
-            <MarketHistory />
-          </Col>
-        </Row>
+            </Col>
+            <Col md='6'>
+              <MarketInfo />
+              <Chart />
+            </Col>
+            <Col md='3'>
+              <OrderBook />
+              <MarketHistory />
+            </Col>
+          </Row>
+        </div>
       </div>
     </div>
   );
